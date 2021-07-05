30926fe39f9e06773cfab1d44a42dea2749a7641
Ilyes1 <ihannouch7@gmail.com>
2021-07-05 11:09:15 +0100

    my changes

--- src/api.js
+++ src/api.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const serverless = require('serverless-http')
+const bodyParser = require('body-parser')
 
 
 const app = express()
 const router = express.Router()
 
 router.use(express.static('dist'))
+router.use(bodyParser.urlencoded({ extended: true }))
+
+router.post('/user', (req, res) => {
+    res.send(req.body.user)
+})
 
 app.use('/.netlify/functions/api', router)
 
